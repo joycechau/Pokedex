e29ee227d00574cea3cbbf67c74d00260d430869
Joyce Chau <jyc.chau@gmail.com>
2017-01-04 14:10:16 -0800

    Add thunk middleware

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import configureStore from './store/store.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
+  // window.fetchAllPokemon = fetchAllPokemon;
+  // window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   const store = configureStore();
   window.store = store;
   const rootEl = document.getElementById('root');
